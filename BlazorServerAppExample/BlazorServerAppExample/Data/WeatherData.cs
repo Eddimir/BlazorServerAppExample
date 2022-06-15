8d215e97e8cae3e030ab431631c657ecd350b8f6
Eddimir Veloz Reynoso <54543106+Eddimir@users.noreply.github.com>
2022-06-15 10:11:37 -0400

    Add files via upload
    Fetching weather data from a service

---
+++ BlazorServerAppExample/BlazorServerAppExample/Data/WeatherData.cs
@@ -0,0 +1,128 @@
+﻿namespace BlazorServerAppExample.Data
+{
+    using System.Text.Json.Serialization;
+
+ 
+    public class WeatherData
+    {
+        [JsonPropertyName("name")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonPropertyName("weather")]
+        public Weather[] Weather { get; set; }
+
+        [JsonPropertyName("base")]
+        public string Base { get; set; }
+
+        [JsonPropertyName("main")]
+        public Main Main { get; set; }
+
+        [JsonPropertyName("visibility")]
+        public long Visibility { get; set; }
+
+        [JsonPropertyName("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonPropertyName("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonPropertyName("dt")]
+        public long Dt { get; set; }
+
+        [JsonPropertyName("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("cod")]
+        public long Cod { get; set; }
+    }
+
+
+    public class Clouds
+    {
+        [JsonPropertyName("all")]
+        public long All { get; set; }
+    }
+
+    public class Coord
+    {
+        [JsonPropertyName("lon")]
+        public double Lon { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double Lat { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonPropertyName("temp")]
+        public double Temperature { get; set; }
+        public int TemperatureF => 32 + (int)(Temperature / 0.5556);
+
+        [JsonPropertyName("pressure")]
+        public long Pressure { get; set; }
+
+        [JsonPropertyName("humidity")]
+        public long Humidity { get; set; }
+
+        [JsonPropertyName("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonPropertyName("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonPropertyName("feels_like")]
+        public double Percepita { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonPropertyName("type")]
+        public long Type { get; set; }
+
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("message")]
+        public double Message { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("sunrise")]
+        public long Sunrise { get; set; }
+
+        [JsonPropertyName("sunset")]
+        public long Sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("main")]
+        public string Visibility { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; set; }
+        public string IconFullUrl => $"http://openweathermap.org/img/wn/{Icon}@2x.png";
+    }
+
+    public class Wind
+    {
+        [JsonPropertyName("speed")]
+        public double Speed { get; set; }
+
+        [JsonPropertyName("deg")]
+        public long Deg { get; set; }
+    }
+}
---
+++ BlazorServerAppExample/BlazorServerAppExample/Data/WeatherForecastService.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace BlazorServerAppExample.Data
+{
+    public class WeatherForecastService
+    {
+        //private static readonly string[] Summaries = new[]
+        //{        
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"   
+        //};
+
+        //public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        //{
+        //    return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = startDate.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    }).ToArray());
+        //}
+        
+     
+
+        public async Task<WeatherData> GetWeatherData(string cityOrCountryName)
+        {
+            if (string.IsNullOrEmpty(cityOrCountryName)) throw new Exception("No valores en blanco");
+
+            WeatherData? weather = null;
+            const string apiKey = "4d8fb5b93d4af21d66a2948710284366";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cityOrCountryName}&appid={apiKey}&units=metric";
+            using (var httpclient = new HttpClient())
+            {
+                try
+                {
+                    using var httpResponse = await httpclient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                    if (httpResponse.IsSuccessStatusCode)
+                    {
+                        using var contentStream = await httpResponse.Content.ReadAsStreamAsync();
+                        try
+                        {
+                            weather = await JsonSerializer.DeserializeAsync<WeatherData?>(contentStream,
+                                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                        }
+                        catch (Exception ex) 
+                        {
+                            throw new Exception(ex.Message);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            return weather;
+        }
+    }
+}
